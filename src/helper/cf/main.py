9801577ad6d36426cf31a9f0826e66dfec231a01
doublewinter0 <erdong.me@gmail.com>
2023-08-06 18:00:01 +0800

    update cfst config
    Signed-off-by: doublewinter0 <erdong.me@gmail.com>

--- src/helper/cf/main.py
+++ src/helper/cf/main.py
@@ -5,8 +5,13 @@ from src.constant import MainConstant, NumberConstant
 
 
 def get_high_quality_ip() -> Tuple:
-    with subprocess.Popen(
-            ['lib/cf/CloudflareST', '-p', '0', '-f', 'lib/cf/ip.txt', '-o', 'lib/cf/result.csv']) as proc:
+    with subprocess.Popen([
+        'lib/cf/CloudflareST',
+        '-url', 'https://cdn.cloudflare.steamstatic.com/steam/apps/256843155/movie_max.mp4',
+        '-p', '0',
+        '-f', 'lib/cf/ip.txt',
+        '-o', 'lib/cf/result.csv'
+    ]) as proc:
         stdout, stderr = proc.communicate()
 
         with open('lib/cf/result.csv', encoding=MainConstant.UTF8) as result:
